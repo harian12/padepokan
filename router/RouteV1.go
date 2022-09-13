f558afb219b4e42299df0539b28a3df8dd3928f1
harian12 <hariansyah434@gmail.com>
2022-09-14 02:18:48 +0700

    add version route

--- router/RouteV1.go
+++ router/RouteV1.go
@@ -5,10 +5,13 @@ import (
 )
 
 func RouteV1(r *gin.Engine) {
-	nasabah := r.Group("nasabah")
+
+	v1 := r.Group("v1")
+	nasabah := v1.Group("nasabah")
 	nasabah.GET("data", nasabah_controller.GetMasabah)
+	nasabah.GET("point", nasabah_controller.GetPointMasabah)
 	nasabah.POST("create", nasabah_controller.CreateNasabah)
 
-	transaction := r.Group("transaction")
+	transaction := v1.Group("transaction")
 	transaction.POST("create", transaction_controller.CreateTransaction)
 }
