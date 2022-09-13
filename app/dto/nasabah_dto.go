2806b6d0c8f1af40bd63f973421028601956f145
harian12 <hariansyah434@gmail.com>
2022-09-14 02:19:52 +0700

    penambahan format response get and get point nasabah

--- app/dto/nasabah_dto.go
+++ app/dto/nasabah_dto.go
@@ -3,3 +3,14 @@ package dto
 type Nasabah struct {
 	Name string `json:"name" validate:"required"`
 }
+
+type ResGetNasabah struct {
+	AccountId uint64 `json:"account_id" `
+	Name      string `json:"name"`
+}
+
+type ResGetPointNasabah struct {
+	AccountId  uint64 `json:"account_id" `
+	Name       string `json:"name"`
+	TotalPoint uint32 `json:"total_point"`
+}
